d01441068ccae5df6fe480950b7d46de4982e954
Steven Greulich <greulich.steven@gmail.com>
2017-09-01 14:30:58 -0400

    Creation of Array for numbers
    Creation of numbers in english for numbers

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,5 +9,8 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"};
+
     }
 }
